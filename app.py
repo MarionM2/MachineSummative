7b47f7be4321e5511a9c776bb1eca150eb5f8e20
marionm2 <m.maina2@alustudent.com>
2023-12-13 11:05:25 +0200

    error handling

--- app.py
+++ app.py
@@ -4,22 +4,31 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import xgboost as xgb
 
 # Load the pre-trained model and vectorizer
-with open("model.pkl", "rb") as file:
-    model_and_vectorizer = pickle.load(file)
-    final_model = model_and_vectorizer['model']
-    vectorizer = model_and_vectorizer['vectorizer']
+try:
+    with open("model.pkl", "rb") as file:
+        model_and_vectorizer = pickle.load(file)
+        final_model = model_and_vectorizer['model']  # Load the trained machine learning model
+        vectorizer = model_and_vectorizer['vectorizer']  # Load the text vectorizer
+except FileNotFoundError:
+    st.error("Error: Model file not found. Please make sure 'model.pkl' is in the correct directory.")
+except Exception as e:
+    st.error(f"An error occurred while loading the model: {e}")
 
 # Function to preprocess input text and vectorize
 def preprocess_and_vectorize(text):
-    preprocessed_text = text.lower()  # Example: Convert to lowercase
-    tfidf_features = vectorizer.transform([preprocessed_text])
+    preprocessed_text = text.lower()  # Convert to lowercase for consistency
+    tfidf_features = vectorizer.transform([preprocessed_text])  # Use the pre-trained vectorizer
     return tfidf_features
 
 # Function to predict
 def predict(text):
-    tfidf_features = preprocess_and_vectorize(text)
-    prediction = final_model.predict(tfidf_features)[0]
-    return prediction
+    try:
+        tfidf_features = preprocess_and_vectorize(text)
+        prediction = final_model.predict(tfidf_features)[0]  # Make a prediction using the loaded model
+        return prediction
+    except Exception as e:
+        st.error(f"An error occurred during prediction: {e}")
+        return None
 
 # Streamlit app
 def main():
@@ -29,11 +38,15 @@ def main():
     user_input = st.text_area("Enter the text to classify")
 
     if st.button("Predict"):
-        if user_input:
-            prediction = predict(user_input)
-            st.write(f"Prediction: {'Spam' if prediction == 1 else 'Not Spam'}")
+        if user_input and isinstance(user_input, str):  # Additional input validation
+            try:
+                prediction = predict(user_input)
+                if prediction is not None:
+                    st.write(f"Prediction: {'Spam' if prediction == 1 else 'Not Spam'}")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
         else:
-            st.warning("Please enter text for prediction.")
+            st.warning("Please enter valid text for prediction.")
 
 if __name__ == "__main__":
     main()
